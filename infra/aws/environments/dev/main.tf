7e6b1783ddcee32a8df8eecf56cbd10d43f8104e
Ranjeet Singh <>
2026-02-27 13:36:44 +0530

    fix deploy runtime: ALB ingress, ECS deploy flow, and latest image tag

--- infra/aws/environments/dev/main.tf
+++ infra/aws/environments/dev/main.tf
@@ -63,6 +63,14 @@ resource "aws_security_group" "ecs_dev" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP traffic to ALB listener"
+  }
+
+  ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
